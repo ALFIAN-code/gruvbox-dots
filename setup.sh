e85ce284e3dee751643dc7002f2e4c0898b7d61e
bagus alfian <allvvnt@gmail.com>
2024-04-16 21:49:53 +0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#instaling depecency
+echo "
+
+
+install dependency ...
+
+"
+
+yay -S kitty grimblast swappy swww swaylock ags neofetch gvfs gnome-bluetooth-3.0 pipwire-pulse NetworkManager hyprland spotify android-studio visual-studio-code-bin waypaper snapd 
+
+
+
+
+
+#download theme
+echo "
+
+
+download theme ...
+
+" 
+git clone .....
+
+#copy theme
+
+
