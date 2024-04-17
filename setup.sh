4d695f626b7d1314bfe69f0a211dc9c4cd8450ad
bagus alfian <allvvnt@gmail.com>
2024-04-17 15:53:44 +0700

    add spicetify

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ cat << "EOF"
 
 EOF
 
-yay -S dolphin fish kitty grimblast swappy swww swaylock ags neofetch gvfs gnome-bluetooth-3.0 pipewire-pulse NetworkManager hyprland spotify android-studio visual-studio-code-bin waypaper snapd grim swappy slurp sddm sddm-kcm qt6-svg qt6-declarative kvantum-qt5 qt5-wayland qt6-wayland qt6ct qt5ct nwg-look blueman 
+yay -S dolphin fish kitty grimblast swappy swww swaylock ags neofetch gvfs gnome-bluetooth-3.0 pipewire-pulse NetworkManager hyprland spotify android-studio visual-studio-code-bin waypaper snapd grim swappy slurp sddm sddm-kcm qt6-svg qt6-declarative kvantum-qt5 qt5-wayland qt6-wayland qt6ct qt5ct nwg-look blueman network-manager-applet curl
 
 
 
@@ -133,6 +133,22 @@ EOF
 
 sudo systemctl enable --now snapd.socket
 sudo systemctl enable --now snapd.apparmor.service
+
+#install spicetify ==========================================================
+cat << "EOF"
+ _           _        _ _             
+(_)_ __  ___| |_ __ _| | |            
+| | '_ \/ __| __/ _` | | |            
+| | | | \__ \ || (_| | | |            
+|_|_| |_|___/\__\__,_|_|_|_  __       
+ ___ _ __ (_) ___ ___| |_(_)/ _|_   _ 
+/ __| '_ \| |/ __/ _ \ __| | |_| | | |
+\__ \ |_) | | (_|  __/ |_| |  _| |_| |
+|___/ .__/|_|\___\___|\__|_|_|  \__, |
+    |_|                         |___/   
+
+EOF
+curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 
 
 #install flutter ============================================================
