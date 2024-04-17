61d224b8d59f3bd06fbc17eb4d03c3f51c76e800
bagus alfian <allvvnt@gmail.com>
2024-04-17 14:53:37 +0700

    update 2

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-
-#instaling depecency ============================================================
 cat << "EOF"
 
 *********************************************************
@@ -20,6 +17,23 @@ cat << "EOF"
 *********************************************************
 EOF
 
+#instaling depecency ============================================================
+cat << "EOF"
+
+                 _       _       
+ _   _ _ __   __| | __ _| |_ ___ 
+| | | | '_ \ / _` |/ _` | __/ _ \
+| |_| | |_) | (_| | (_| | ||  __/
+ \__,_| .__/ \__,_|\__,_|\__\___|
+ ___ _|_|_ ___| |_ ___ _ __ ___  
+/ __| | | / __| __/ _ \ '_ ` _ \ 
+\__ \ |_| \__ \ ||  __/ | | | | |
+|___/\__, |___/\__\___|_| |_| |_|
+     |___/                       
+
+
+EOF
+
 sudo pacman -Syyu
 
 
@@ -36,9 +50,11 @@ cat << "EOF"
 | (_| |  __/ |_) |  __/ | | | (_| |  __/ | | | (__| |_| |
  \__,_|\___| .__/ \___|_| |_|\__,_|\___|_| |_|\___|\__, |
            |_|                                     |___/ 
+
+
 EOF
 
-yay -S dolphin fish kitty grimblast swappy swww swaylock ags neofetch gvfs gnome-bluetooth-3.0 pipewire-pulse NetworkManager hyprland spotify android-studio visual-studio-code-bin waypaper snapd grim swappy slurp sddm sddm-kcm qt6-svg qt6-declarative
+yay -S dolphin fish kitty grimblast swappy swww swaylock ags neofetch gvfs gnome-bluetooth-3.0 pipewire-pulse NetworkManager hyprland spotify android-studio visual-studio-code-bin waypaper snapd grim swappy slurp sddm sddm-kcm qt6-svg qt6-declarative kvantum-qt5 qt5-wayland qt6-wayland qt6ct qt5ct nwg-look blueman 
 
 
 
@@ -53,23 +69,36 @@ cat << "EOF"
 | | | | \__ \ || (_| | | |    | |_| | | |  __/ | | | | |  __/
 |_|_| |_|___/\__\__,_|_|_|     \__|_| |_|\___|_| |_| |_|\___|
 
+
 EOF
+
 cd hyprland-gruvbox-dots
 
 cp -r .config/* ~/.config/
 cp -r fonts/* ~/.local/share/fonts/
 cp -r catppuccin-mocha /usr/share/sddm/themes/
 
-sed -i 's/kata_lama/kata_baru/g' nama_file
+sudo sh -c 'echo "
+[Theme]
+Current=catppuccin-mocha
+" >> /etc/sddm.conf'
 
 
 #install fish ============================================================
-echo "
+cat << "EOF"
+                        __ _                       
+  ___ ___  _ __  _ __  / _(_) __ _ _   _ _ __ ___  
+ / __/ _ \| '_ \| '_ \| |_| |/ _` | | | | '__/ _ \ 
+| (_| (_) | | | | | | |  _| | (_| | |_| | | |  __/ 
+ \___\___/|_| |_|_| |_|_| |_|\__, |\__,_|_|  \___| 
+ / _(_)___| |__    ___| |__  |___/ | |             
+| |_| / __| '_ \  / __| '_ \ / _ \ | |             
+|  _| \__ \ | | | \__ \ | | |  __/ | |             
+|_| |_|___/_| |_| |___/_| |_|\___|_|_|   
 
 
-changing shell to fish ...
+EOF
 
-" 
 chsh -s $(which fish)
 
 echo "
@@ -84,23 +113,38 @@ fi
 
 
 #configure snapd ============================================================
-echo "
+cat << "EOF"
+                                               
+                        __ _                       
+  ___ ___  _ __  _ __  / _(_) __ _ _   _ _ __ ___  
+ / __/ _ \| '_ \| '_ \| |_| |/ _` | | | | '__/ _ \ 
+| (_| (_) | | | | | | |  _| | (_| | |_| | | |  __/ 
+ \___\___/|_| |_|_| |_|_| |_|\__, |\__,_|_|  \___| 
+ ___ _ __   __ _ _ __   __| ||___/                 
+/ __| '_ \ / _` | '_ \ / _` |                      
+\__ \ | | | (_| | |_) | (_| |                      
+|___/_| |_|\__,_| .__/ \__,_|                      
+                |_|                                   
 
 
-configure snapd ...
 
-" 
+EOF
+
+
 sudo systemctl enable --now snapd.socket
 sudo systemctl enable --now snapd.apparmor.service
 
 
 #install flutter ============================================================
-echo "
+cat << "EOF"
+ _           _        _ _    __ _       _   _            
+(_)_ __  ___| |_ __ _| | |  / _| |_   _| |_| |_ ___ _ __ 
+| | '_ \/ __| __/ _` | | | | |_| | | | | __| __/ _ \ '__|
+| | | | \__ \ || (_| | | | |  _| | |_| | |_| ||  __/ |   
+|_|_| |_|___/\__\__,_|_|_| |_| |_|\__,_|\__|\__\___|_|   
 
+EOF
 
-install flutter ...
-
-" 
 snap install flutter --classic
 flutter sdk-path
 
